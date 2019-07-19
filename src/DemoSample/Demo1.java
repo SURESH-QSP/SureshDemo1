fb4aa4d13eac79c029f4d13e1a069fbe1939031a
Admin <Admin@DESKTOP-LSOM5AR>
2019-07-19 19:48:42 +0530

    2nd Demo

--- src/DemoSample/Demo1.java
+++ src/DemoSample/Demo1.java
@@ -1,10 +1,11 @@
 package DemoSample;
 
-public class Demo1 {
+public class Demo1
+{
 
-	public static void main(String[] args) {
-		System.out.println("Hi Everyone");
+	public static void main(String[] args)
+	{
+		System.out.println(" i am good ");
 
 	}
-
 }
